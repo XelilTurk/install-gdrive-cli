421d9d2dd00823541e7f3b03ad94631f9101ce81
Tuhin Kanti Pal <51857187+cachecleanerjeet@users.noreply.github.com>
2020-07-12 15:43:40 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+cd /usr/sbin
+wget https://github.com/cachecleanerjeet/install-gdrive-cli/raw/upload/gdrive
+chmod +rwx gdrive
+echo 'Successfully Installed GDrive CLI'
+echo 'Visit https://tu.hin.life'
